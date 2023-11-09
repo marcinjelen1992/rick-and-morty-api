a7d30c98d1ca8ce4d8617dc833bd1fb98d2758e7
marcinjelen1992 <marcin.jelen.1992@gmail.com>
2023-11-09 20:49:11 +0100

    First branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
+
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const API_URL = 'https://rickandmortyapi.com/api/character?page=1';
+
+function App(): JSX.Element {
+  interface Character {
+    id: number;
+    name: string;
+    image: string;
+ }
+
+  const [character, setCharacter] = useState<Character[]>([]);
+
+
+
+  useEffect(() => {
+    async function fetchResponse() {
+      try {
+  
+      
+      const response = await fetch(API_URL);
+      const { results } = await response.json();
+  
+      setCharacter(results)
+    } catch (error) {return error}
+  }
+    fetchResponse()
+      }, [])
+
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Rick and Morty</h1>
+      <div>{
+            character.map(character => {
+                return (
+                    <div key={character.id}>
+                        <p>Name: {character.name}</p>
+                        <img src={character.image} alt="" />
+                    </div>
+                )
+            })
+        }</div>
     </>
   )
 }
